d3f1a4e0eac6356dc795cbc0c0c8a1940362f366
samrasugu <mokuasamr@gmail.com>
2023-10-16 09:51:02 +0300

    chore: fixed crypt functions

--- helpers/cryptfunctions.js
+++ helpers/cryptfunctions.js
@@ -1,18 +1,20 @@
+require("dotenv").config();
 const crypto = require("crypto");
+const buffer = require("buffer")
 
 const algorithm = "aes-256-cbc";
-const key = crypto.randomBytes(32);
-const iv = crypto.randomBytes(16);
+const key = process.env.ENCRYPTION_KEY;
+const iv = Buffer.from(process.env.ENCRYPTION_IV, 'hex');
 
 function encrypt(text) {
-  const cipher = crypto.createCipheriv(algorithm, key, iv);
+  const cipher = crypto.createCipheriv(algorithm, Buffer.from(key, 'hex'), iv);
   let encrypted = cipher.update(text, "utf8", "hex");
   encrypted += cipher.final("hex");
   return encrypted;
 }
 
 function decrypt(encrypted) {
-  const decipher = crypto.createDecipheriv(algorithm, key, iv);
+  const decipher = crypto.createDecipheriv(algorithm, Buffer.from(key, 'hex'), iv);
   let decrypted = decipher.update(encrypted, "hex", "utf8");
   decrypted += decipher.final("utf8");
   return decrypted;
